0ccf5b24a62bfde421426d6c74666393fc42e4d4
Edenilson JP <52428137+ninojp@users.noreply.github.com>
2026-06-11 02:35:57 -0300

    Movi node_modules e Package,json para Raiz do Projeto Principal

--- DtudoSite/src/api_conect/conectApiTMDB.js
+++ DtudoSite/src/api_conect/conectApiTMDB.js
@@ -22,7 +22,7 @@ export default conectaApiTMDB;
 // conectaApiTMDB('1241982-moana-2').then(filme => console.log(filme)).catch(error => console.error(error));
 // async function conectaApiTMDB() {
 //     try {
-//         const response = await fetch(`https://api.themoviedb.org/3/movie/1241982-moana-2?api_key=38574258670385dca5e0428ff5430190&language=pt-BR`);
+//         const response = await fetch(`https://api.themoviedb.org/3/movie/1241982-moana-2?api_key=38574250000ca5e0428ff5430190&language=pt-BR`);
 //         if (!response.ok) {
 //             throw new Error(`conectaApiTMDB(): Erro na requisição: ${response.statusText}`);
 //         };
--- DtudoSite/vite.config.js
+++ DtudoSite/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+//adicionado após mudança do package.json (e node_modules) para o diretório raiz do projeto, para evitar problemas de path com o vite.config.js
+const projectRoot = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: projectRoot,
   plugins: [
     react({
       babel: {
